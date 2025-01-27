bacd69d30d0081cd32d7b196b412f4f866b2c9fd
kimyoungseok3232 <rpfps3232@gmail.com>
2025-01-27 10:34:25 +0900

    Feat: 모델 동작 구조 변경 피쳐 값 음수로 내려가지 않음 랜덤 피쳐에서 시작 학습시 로스에 reward 값 곱해서 실제 스테이트와 달라도 비슷한 결과 나오면 학습하지 않음

--- hackathon/src/search/RL_search.py
+++ hackathon/src/search/RL_search.py
@@ -10,7 +10,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-def RL_search(model):
+def RL_search(surrogate_model):
     def set_seed(seed=42):
         np.random.seed(seed)
         random.seed(seed)
@@ -20,9 +20,9 @@ def RL_search(model):
         torch.backends.cudnn.benchmark = False
     set_seed(3232)
 
-    class DQNetwork(nn.Module):
+    class search_model(nn.Module):
         def __init__(self, input_dim, output_dim):
-            super(DQNetwork, self).__init__()
+            super(search_model, self).__init__()
             self.fc = nn.Sequential(
                 nn.Linear(input_dim, 128),
                 nn.ReLU(),
@@ -36,7 +36,6 @@ def RL_search(model):
                 nn.ReLU(),
                 nn.Linear(64, output_dim)
             )
-
         def forward(self, x):
             return self.fc(x)
 
@@ -58,8 +57,8 @@ def RL_search(model):
     # DQN 에이전트 설정
     input_dim = train_data.shape[1]  # feature 개수
     action_dim = input_dim - 1  # 각 feature에 대한 액션
-    q_network = DQNetwork(input_dim, action_dim).to(device)  # +1은 타겟 strength 포함
-    optimizer = optim.Adam(q_network.parameters(), lr=1e-3, weight_decay=0.1)
+    search_network = search_model(input_dim, action_dim).to(device)  # +1은 타겟 strength 포함
+    optimizer = optim.Adam(search_network.parameters(), lr=1e-3, weight_decay=0.1)
     criterion = nn.MSELoss()
 
     # 학습 설정
@@ -72,7 +71,7 @@ def RL_search(model):
     val_mean_reward = []
     val_min_mean_reward = []
     for epoch in range(num_epochs):
-        q_network.train()
+        search_network.train()
         train_loss_sum = 0.0
         train_loss_count = 0
         train_total_reward = 0.0
@@ -83,32 +82,34 @@ def RL_search(model):
             features = batch.iloc[:, :-1].values
             actual_strengths = batch['strength'].values
 
-            initial_state = np.repeat(np.expand_dims(init, axis=0), features.shape[0], axis=0)
+            initial_state = np.array([init(df.iloc[:,:-1]) for i in range(features.shape[0])])
 
             # LightGBM 예측
             scaled_initial_state = scaler.transform(pd.DataFrame(initial_state, columns=scaler.feature_names_in_))
-            predicted_strengths = model.predict(scaled_initial_state)
+            predicted_strengths = surrogate_model.predict(scaled_initial_state)
 
-            # DQN 입력 구성
-            dqn_input = np.hstack([initial_state, actual_strengths[:,np.newaxis] - predicted_strengths[:,np.newaxis]])
-            dqn_input_tensor = torch.tensor(dqn_input, dtype=torch.float32).to(device)
+            # search model 입력 구성
+            search_input = np.hstack([initial_state, actual_strengths[:,np.newaxis] - predicted_strengths[:,np.newaxis]])
+            search_input_tensor = torch.tensor(search_input, dtype=torch.float32).to(device)
 
             # 액션 예측
-            predicted_actions_tensor = q_network(dqn_input_tensor)
+            predicted_actions_tensor = search_network(search_input_tensor)
 
             # 실제 액션 계산 (실제 feature - 초기 상태)
             actual_actions_tensor = torch.tensor(scaler.inverse_transform(features) - initial_state, dtype=torch.float32).to(device)
 
             # 업데이트된 상태 계산
-            updated_state = initial_state + predicted_actions_tensor.cpu().detach().numpy()
+            updated_state = nn.ReLU()(torch.tensor(initial_state).to(device) + predicted_actions_tensor).cpu().detach().numpy()
             scaled_updated_state = scaler.transform(pd.DataFrame(updated_state, columns=scaler.feature_names_in_))
-            updated_strengths = model.predict(scaled_updated_state)
+            updated_strengths = surrogate_model.predict(scaled_updated_state)
 
             # 리워드 계산
             rewards = updated_strengths - actual_strengths  # 리워드 계산
-            rewards_tensor = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1)
+            rewards_tensor = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(device)
 
-            # DQN 손실 계산 (리워드 활용)
+            # search model 손실 계산 (리워드 활용)
+            predicted_actions_tensor *= rewards_tensor
+            actual_actions_tensor *= rewards_tensor
             loss = criterion(predicted_actions_tensor, actual_actions_tensor)
             #loss = (loss * rewards_tensor.mean())  # 리워드를 손실에 반영
 
@@ -131,7 +132,7 @@ def RL_search(model):
         traim_mean_loss.append(train_loss_mean)
         train_mean_reward.append(train_reward_mean)
         # ===== Validation =====
-        q_network.eval()
+        search_network.eval()
         val_loss_sum = 0.0
         val_loss_count = 0
         val_total_reward = 0.0
@@ -145,32 +146,34 @@ def RL_search(model):
                 actual_strengths = batch['strength'].values
 
                 # 초기 상태를 중간값으로 설정
-                initial_state = np.repeat(np.expand_dims(init, axis=0), features.shape[0], axis=0)
+                initial_state = np.array([init(df.iloc[:,:-1]) for i in range(features.shape[0])])
                 re = np.full(features.shape[0], 100)
                 for j in range(10):
                     scaled_initial_state = scaler.transform(pd.DataFrame(initial_state, columns=scaler.feature_names_in_))
-                    predicted_strengths = model.predict(scaled_initial_state)
+                    predicted_strengths = surrogate_model.predict(scaled_initial_state)
 
-                    # DQN 입력 구성
-                    dqn_input = np.hstack([initial_state, actual_strengths[:,np.newaxis] - predicted_strengths[:,np.newaxis]])
-                    dqn_input_tensor = torch.tensor(dqn_input, dtype=torch.float32).to(device)
+                    # search model 입력 구성
+                    search_input = np.hstack([initial_state, actual_strengths[:,np.newaxis] - predicted_strengths[:,np.newaxis]])
+                    search_input_tensor = torch.tensor(search_input, dtype=torch.float32).to(device)
 
-                    # DQN으로 액션 예측
-                    predicted_actions_tensor = q_network(dqn_input_tensor)
+                    # search model로 액션 예측
+                    predicted_actions_tensor = search_network(search_input_tensor)
 
                     # 실제 액션 계산 (실제 feature - 초기 상태)
                     actual_actions_tensor = torch.tensor(scaler.inverse_transform(features) - initial_state, dtype=torch.float32).to(device)
 
                     # 업데이트된 상태 계산
-                    updated_state = initial_state + predicted_actions_tensor.cpu().detach().numpy()
+                    updated_state = nn.ReLU()(torch.tensor(initial_state).to(device) + predicted_actions_tensor).cpu().detach().numpy()
                     scaled_updated_state = scaler.transform(pd.DataFrame(updated_state, columns=scaler.feature_names_in_))
-                    updated_strengths = model.predict(scaled_updated_state)
+                    updated_strengths = surrogate_model.predict(scaled_updated_state)
 
                     # 리워드 계산
                     rewards = updated_strengths - actual_strengths
-                    rewards_tensor = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1)
+                    rewards_tensor = torch.tensor(rewards, dtype=torch.float32).unsqueeze(1).to(device)
 
-                    # DQN 손실 계산
+                    # search model 손실 계산
+                    predicted_actions_tensor *= rewards_tensor
+                    actual_actions_tensor *= rewards_tensor
                     loss = criterion(predicted_actions_tensor, actual_actions_tensor)
                     #loss = (loss * rewards_tensor.mean())
                     
@@ -190,29 +193,29 @@ def RL_search(model):
         val_min_mean_reward.append(val_min_reward_mean)
         if val_min_reward_mean < min_rew: 
             min_rew = val_min_reward_mean
-            torch.save(q_network.state_dict(), './model.pth') 
+            torch.save(search_network.state_dict(), './model.pth') 
         # Epoch 결과 출력
         print(f"Epoch {epoch + 1}/{num_epochs}\n"
             f"Train Loss Mean: {train_loss_mean:.4f}, Train Total Reward: {train_total_reward:.4f}, Train Mean Reward: {train_reward_mean:.4f}, "
             f"Val Loss Mean: {val_loss_mean:.4f}, Val Total Reward: {val_total_reward:.4f}, Val Mean Reward: {val_reward_mean:.4f}, Val min Mean Reward: {val_min_reward_mean:.4f}")
-    test(df, 7, model, q_network, scaler, init, device)
+    test(df, 7, surrogate_model, search_network, scaler, device)
 
-def test(df, idx, model, q_network, scaler, init, device):
-    idx = 230
+def test(df, idx, surrogate_model, search_network, scaler, device):
+    init_in = np.array(df.iloc[:,:-1].mean().values)
     tar_d = df.iloc[idx, :-1]
     tar = df.iloc[idx]['strength']
-    print(f'y: {tar}, x: {tar_d}')
 
-    now = model.predict(scaler.transform(pd.DataFrame(init.reshape(1,8), columns=scaler.feature_names_in_)))
-    input = np.hstack([init.reshape(1,8),np.array([tar])[:,np.newaxis] - now[:,np.newaxis]])
+    now = surrogate_model.predict(scaler.transform(pd.DataFrame(init_in.reshape(1,8), columns=scaler.feature_names_in_)))
+    input = np.hstack([init_in.reshape(1,8),np.array([tar])[:,np.newaxis] - now[:,np.newaxis]])
     input_tensor = torch.tensor(input, dtype=torch.float32).to(device)
-    ac = q_network(input_tensor).cpu().detach().numpy()
+    ac = search_network(input_tensor).cpu().detach().numpy()
     print(f'예측 된 액션: {ac}')
-    fe = ac+init.reshape(1,8)
-    print(f'최종 x: {fe}')
-    init_fe = scaler.transform(pd.DataFrame(init.reshape(1,8), columns=scaler.feature_names_in_))
-    init_st = model.predict(init_fe)
+    ofe = ac+init_in.reshape(1,8)
+    fe = np.maximum(0,ofe)
+    print(f'최종 x:\n{pd.DataFrame(fe, columns=df.columns[:-1]).iloc[0]}')
+    init_fe = scaler.transform(pd.DataFrame(init_in.reshape(1,8), columns=scaler.feature_names_in_))
+    init_st = surrogate_model.predict(init_fe)
     up_fe = scaler.transform(pd.DataFrame(fe, columns=scaler.feature_names_in_))
-    up_st = model.predict(up_fe)
-    print(f'목표 x: {tar_d}')
+    up_st = surrogate_model.predict(up_fe)
+    print(f'목표 x:\n{tar_d}')
     print(f'초기 상태 y: {init_st}, 액션 이후 y: {up_st}, 목표 y: {tar}')
