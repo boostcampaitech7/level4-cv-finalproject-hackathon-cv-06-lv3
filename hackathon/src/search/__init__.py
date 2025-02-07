d3222aee5a4ea57b706ab20b9fb9ea260c49c0ae
taes <randfo42@gmail.com>
2025-02-07 12:11:47 +0900

    refactor : search

--- hackathon/src/search/__init__.py
+++ hackathon/src/search/__init__.py
@@ -7,3 +7,9 @@ from .ga_deap_search import ga_deap_search
 from .ga_adaptive_niching_search import ga_adaptive_niching_search
 from .k_means_search import k_means_search
 from .k_means_search_deploy import k_means_search_deploy
+
+from .ga_func import cx_simulated_binary_w_cx_uniform,\
+                    mutGaussian_mutUniformInt,\
+                    k_means_selection,\
+                    lexicographic_selection 
+
